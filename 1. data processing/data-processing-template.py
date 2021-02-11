21cbf1fd7efc13ce929c9eb10b85e5c1da4af4df
LordAldi <aldianugra09@gmail.com>
2021-02-11 12:58:24 +0700

    feature scalling

--- 1. data processing/data-processing-template.py
+++ 1. data processing/data-processing-template.py
@@ -32,3 +32,9 @@ Y = LabelEncoder().fit_transform(Y)
 #splitting datatset into training aand the test set
 from sklearn.model_selection import train_test_split 
 X_train, X_test, Y_train, Y_test =  train_test_split(X, Y,test_size = 0.2, random_state= 0)
+
+#feature scalling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
