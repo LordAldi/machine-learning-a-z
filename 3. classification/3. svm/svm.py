e7525bc5b717f19df20abf9686ad6d8261b339d2
LordAldi <aldianugra09@gmail.com>
2021-02-14 07:27:57 +0700

    svm

--- 3. classification/3. svm/svm.py
+++ 3. classification/3. svm/svm.py
@@ -51,7 +51,7 @@ plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).re
 
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
-
+ 
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
